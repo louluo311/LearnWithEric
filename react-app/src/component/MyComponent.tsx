e3bfea6c936a9f992328a7a4bfb55575e83ce3fa
Loi Luong <loiluong@Lois-MacBook-Pro.local>
2023-06-10 10:10:14 -0700

    video 19

--- react-app/src/component/MyComponent.tsx
+++ react-app/src/component/MyComponent.tsx
@@ -6,10 +6,20 @@ class MyComponent extends React.Component {
         address: 'Martial Art Dojo',
         age: 25
     }
+    handleClick() {
+        // console.log(event.target)
+        console.log('My name is ', this.state.name)
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event.pageX)
+    }
     render() {
         return (
             <div>
-                my name is {this.state.name}
+                my name is {this.state.name} and I'm from {this.state.address}
+                <button onClick={this.handleClick}>Click Me</button>
+                <button onMouseOver={this.handleOnMouseOver}>Hover Me</button>
             </div>
         );
     }
