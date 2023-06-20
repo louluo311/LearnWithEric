214cf0c1e68d1c187a13b0c44c5a442cfec0d5e9
Loi Luong <loiluong@Lois-MacBook-Pro.local>
2023-06-20 14:21:39 -0700

    video 23

--- react-app/src/component/MyComponent.tsx
+++ react-app/src/component/MyComponent.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
     
     render() {
+        const myInfor= ['ab', 'c','d']
         return (
             <div>
                 <UserInfor></UserInfor>
-                
-
+                <br /><br />
+                <DisplayInfor name='Hoi Dan IT' age='27' />
+                <hr />
+                <DisplayInfor name='Loi Luong' age={30} myInfor={myInfor} />
             </div>
         );
     }
