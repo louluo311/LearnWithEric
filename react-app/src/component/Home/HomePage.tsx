7072135dd10590445d588268968789415cd66e29
Loi Luong <loiluong@Lois-MacBook-Pro.local>
2023-07-12 13:12:11 -0700

    video 51 and 52

--- react-app/src/component/Home/HomePage.tsx
+++ react-app/src/component/Home/HomePage.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import videoHomePage from '../assets/video-homepage.mp4'
 
 const HomePage = (props) => {
     return (
-        <div className='homepage-container'>Home Page container</div>
+        <div className='homepage-container'>Home Page container
+            <video autoPlay muted loop >
+                <source
+                    src={videoHomePage}
+                    type="video/webm" />
+            </video>
+        </div>
     )
 }
 
