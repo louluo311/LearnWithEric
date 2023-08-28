f7a802bdf9c88a0ae488484df74b79e53e47fead
Loi Luong <loiluong@Lois-MacBook-Pro.local>
2023-08-28 12:16:52 -0700

    video 53

--- react-app/src/component/Header/Header.tsx
+++ react-app/src/component/Header/Header.tsx
@@ -19,14 +19,16 @@ const Header = () => {
                         <NavLink to='/admin' className='nav-link'>Admin</NavLink>
                     </Nav>
                     <Nav>
-                        <NavDropdown title="Setting" id="basic-nav-dropdown">
+                        <button className='btn-login'>Log in</button>
+                        <button className='btn-signup'>Sign up</button>
+                        {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
                             <NavDropdown.Item >
                                 Log In
                             </NavDropdown.Item>
                             <NavDropdown.Item >
                                 Log Out
                             </NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
 
                 </Navbar.Collapse>
